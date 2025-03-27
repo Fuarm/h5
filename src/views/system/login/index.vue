b296eae21708cb4813227e5d17fc222ac8637361
Fuarm <51748206+Fuarm@users.noreply.github.com>
2025-03-27 09:20:05 +0800

    Update index.vue

--- src/views/system/login/index.vue
+++ src/views/system/login/index.vue
@@ -1,146 +1,96 @@
 <script setup>
-  import { computed, ref, shallowRef } from "vue";
+  import { ref, shallowRef, computed, onMounted } from "vue";
+
+  import Icon from "@/assets/icons/ico-hetong.svg";
+  import anime from "animejs";
 
   const props = defineProps({
-    radius: { type: Number, default: 160 }, // 外圆半径
-    innerRadius: { type: Number, default: 85 }, // 内圆半径
-    thickness: { type: Number, default: 20 }, // 扇形厚度
-    segments: { type: Array, default: () => new Array(6).fill(40) }, // 扇形角度
+    radius: { type: Number, default: 120 }, // 外圆半径
+    innerRadius: { type: Number, default: 60 }, // 内圆半径
+    segmentAngle: { type: Number, default: 40 },
+    segments: {
+      type: Array,
+      default: () => new Array(4).fill({ icon: Icon, label: "合同" })
+    }, // 扇形角度
     gap: { type: Number, default: 8 }, // 间隔角度
-    icons: { type: Array, default: () => ["icon1.png", "icon2.png", "icon3.png", "icon4.png"] }, // 图标路径
-    labels: { type: Array, default: () => ["A", "B", "C", "D"] } // 文字标签
+    name: { type: [String, Array], default: () => ["项目", "中心"] },
+    z: { type: Number, default: 9 }
   });
 
-  const totalAngle = computed(() => props.segments.reduce((sum, s) => sum + s, 0));
+  defineEmits(["segment-tap"]);
+
+  // svg 元素
+  const assistiveTouchRef = ref(null);
+  // 圆心坐标
+  let assistiveTouchCenterCoords = shallowRef({ x: 0, y: 0 });
+  const style = computed(() => ({
+    transformOrigin: `${props.radius - 20}px ${props.radius}px`
+  }));
+
+  // 显示状态
+  let visible = ref(false);
 
   // 计算扇形路径和文字/icon 位置
-  let paths = shallowRef([]);
+  const paths = shallowRef([]);
 
-  const originStartAngle = -180 + totalAngle.value / 2 - props.gap / 2;
-  let startAngle = ref(originStartAngle); // 初始角度
-  let startTouchAngle = 0; // 手指按下时的角度
-  let deltaAngle = 0; // 当前拖动的角度变化
-  const maxOffsetAngle = computed(() => -102.5 - startAngle.value);
-  let angleOffset = 0; // 基于拖动角度的偏移
-
-  console.log("最大偏移量", maxOffsetAngle.value);
-  // 计算扇形路径和图标位置
-  const updatePaths = () => {
-    angleOffset = startAngle.value + deltaAngle; // 基于拖动角度的偏移
-
-    paths.value = props.segments.map((segment, index) => {
-      const adjustedAngle = segment - props.gap;
-      const endAngle = angleOffset - adjustedAngle;
-
-      // 计算扇形的中心角度
-      const centerAngle = (angleOffset + endAngle) / 2;
-      const centerRadius = (props.radius + props.innerRadius) / 2; // 取扇形的中间位置
-
-      // 计算 icon 和文字的中心点
-      const centerX = props.radius + Math.cos(toRadians(centerAngle)) * centerRadius;
-      const centerY = props.radius + Math.sin(toRadians(centerAngle)) * centerRadius;
-
-      // 计算扇形路径
-      const outerStartX = props.radius + Math.cos(toRadians(angleOffset)) * props.radius;
-      const outerStartY = props.radius + Math.sin(toRadians(angleOffset)) * props.radius;
-      const outerEndX = props.radius + Math.cos(toRadians(endAngle)) * props.radius;
-      const outerEndY = props.radius + Math.sin(toRadians(endAngle)) * props.radius;
-
-      const innerStartX = props.radius + Math.cos(toRadians(endAngle)) * props.innerRadius;
-      const innerStartY = props.radius + Math.sin(toRadians(endAngle)) * props.innerRadius;
-      const innerEndX = props.radius + Math.cos(toRadians(angleOffset)) * props.innerRadius;
-      const innerEndY = props.radius + Math.sin(toRadians(angleOffset)) * props.innerRadius;
-
-      const pathData = `
-        M ${outerStartX} ${outerStartY}
-        A ${props.radius} ${props.radius} 0 0 0 ${outerEndX} ${outerEndY}
-        L ${innerStartX} ${innerStartY}
-        A ${props.innerRadius} ${props.innerRadius} 0 0 1 ${innerEndX} ${innerEndY}
-        Z
-      `;
-
-      angleOffset -= segment; // 逆时针调整角度
-      return {
-        pathData,
-        centerX,
-        centerY,
-        icon: props.icons[index],
-        label: props.labels[index],
-        index
-      };
-    });
-  };
+  // 扇形渲染起始角度 startAngel 取值范围 [-102.5, -102.5 + (props.segments.length-4) * props.segmentAngle]
+  const initStartAngle = -102.5;
+  const initEndAngle = -102.5 + (props.segments.length - 4) * props.segmentAngle;
+  let startAngle = initStartAngle;
+  let sealTimeStartAngle = initStartAngle;
+  // 最大偏移角度
+  let maxOffsetAngle = 0;
+  // 当前拖动的角度变化
+  let deltaAngle = 0;
+  // 基于拖动角度的偏移
+  let offsetAngle = 0;
+  // 初始触摸角度
+  let startTouchAngle = 0;
 
   // 角度转换为弧度
   const toRadians = (angle) => (angle * Math.PI) / 180;
 
-  // 手指按下事件，记录初始角度
-  const onTouchStart = (event) => {
-    startTouchAngle = getTouchAngle(event); // 获取初始触摸角度
+  // 计算手指相对圆心角度
+  const computedTouchAngle = (touch) => {
+    // 计算触摸点到圆心的角度
+    const dx = touch.clientX - assistiveTouchCenterCoords.value.x;
+    const dy = touch.clientY - assistiveTouchCenterCoords.value.y;
+    let angle = Math.atan2(dy, dx) * (180 / Math.PI); // 计算角度
+
+    return angle >= 0 ? angle : angle + 360; // 将负角度转换为正角度
   };
 
-  // 计算触摸角度并保证其平滑
-  const getTouchAngle = (event) => {
-    const touch = event.touches?.[0] || event.changedTouches[0];
-    const centerX = props.radius;
-    const centerY = props.radius;
-    const dx = touch.clientX - centerX;
-    const dy = touch.clientY - centerY;
-    return Math.atan2(dy, dx) * (180 / Math.PI); // 转换为角度
-  };
-
-  let lastTouchAngle = null;
-  let lastClockwiseOrAnticlockwise = null;
-  // 手指移动事件，计算拖动角度
-  const onTouchMove = (event) => {
-    let currentTouchAngle = getTouchAngle(event);
-
-    let offsetAngle;
-
-    if (lastTouchAngle && lastTouchAngle > 0 && currentTouchAngle < 0) {
-      offsetAngle = 360 + currentTouchAngle - startTouchAngle;
-    } else if (lastTouchAngle && lastTouchAngle < 0 && currentTouchAngle > 0) {
-      offsetAngle = -360 + currentTouchAngle - startTouchAngle;
-    } else {
-      offsetAngle = currentTouchAngle - startTouchAngle;
-      lastTouchAngle = currentTouchAngle;
-    }
-
-    // 顺时针
-    if (getClockwiseOrAnticlockwise(startTouchAngle, currentTouchAngle) === "clockwise") {
-      // 判读起点 - 滑动重点
-    } else {
-      // 逆时针
-    }
-
-    // 限制最大旋转角度
-    if (Math.abs(offsetAngle) > Math.abs(maxOffsetAngle.value)) {
-      deltaAngle =
-        maxOffsetAngle.value * Math.sign(-offsetAngle) +
-        (offsetAngle - maxOffsetAngle.value * Math.sign(-offsetAngle)) * 0.3;
-    } else {
-      deltaAngle = offsetAngle;
-    }
-
-    updatePaths(); // 更新路径和图标位置
+  // 渲染起始位置是否在区间中
+  const isAngleInRange = () => {
+    return !(sealTimeStartAngle <= initStartAngle || sealTimeStartAngle >= initEndAngle);
   };
 
   // 回弹的平滑效果
   const easeOut = (t) => {
-    return 1 - Math.pow(1 - t, 4); // 使回弹速度逐渐减缓
+    return 1 - Math.pow(1 - t, 3); // 使回弹速度逐渐减缓
   };
 
-  // 回弹状态
-  let isRebounding = false; // 是否处于回弹状态
-  let reboundStartAngle = 0; // 回弹开始的角度
-  let reboundTargetAngle = 0; // 回弹目标角度
-  let reboundProgress = 0; // 回弹进度（0到1之间）
+  // 事件结束初始化初始化状态
+  const afterByTouchEnd = () => {
+    startAngle = sealTimeStartAngle;
+    deltaAngle = 0;
+    maxOffsetAngle = 0;
+  };
 
-  // 回弹动画函数
-  const animateRebound = () => {
+  /**
+   * 回弹动画函数
+   * @param config
+   * isRebounding 是否处于回弹状态
+   * reboundStartAngle 回弹开始的角度
+   * reboundTargetAngle 回弹目标角度
+   * reboundProgress 回弹进度（0到1之间）
+   */
+  const animateRebound = (config) => {
+    let { isRebounding, reboundStartAngle, reboundTargetAngle, reboundProgress } = config;
+
     if (!isRebounding) return; // 如果没有进入回弹状态，则不继续动画
 
-    reboundProgress += 0.03; // 控制回弹的速率
+    reboundProgress += 0.05; // 控制回弹的速率
     if (reboundProgress >= 1) {
       reboundProgress = 1;
       isRebounding = false; // 回弹结束
@@ -158,79 +108,156 @@
     updatePaths(); // 更新路径
 
     if (isRebounding) {
-      requestAnimationFrame(animateRebound); // 继续动画
+      requestAnimationFrame(() =>
+        animateRebound({ isRebounding, reboundStartAngle, reboundTargetAngle, reboundProgress })
+      ); // 继续动画
     } else {
-      startAngle.value = startAngle.value + maxOffsetAngle.value;
+      sealTimeStartAngle =
+        Math.abs(initStartAngle - sealTimeStartAngle) <= Math.abs(initEndAngle - sealTimeStartAngle)
+          ? initStartAngle
+          : initEndAngle;
+      afterByTouchEnd();
     }
   };
 
+  // 计算扇形路径和图标位置
+  const updatePaths = () => {
+    sealTimeStartAngle = startAngle + deltaAngle; // 基于拖动角度的偏移
+    offsetAngle = sealTimeStartAngle;
+
+    paths.value = props.segments.map((segment, index) => {
+      const adjustedAngle = props.segmentAngle - props.gap;
+      const endAngle = offsetAngle - adjustedAngle;
+
+      // 计算扇形的中心角度
+      const centerAngle = (offsetAngle + endAngle) / 2;
+      const centerRadius = (props.radius + props.innerRadius) / 2; // 取扇形的中间位置
+
+      // 计算 icon 和文字的中心点
+      const centerX = props.radius + Math.cos(toRadians(centerAngle)) * centerRadius;
+      const centerY = props.radius + Math.sin(toRadians(centerAngle)) * centerRadius;
+
+      // 计算扇形路径
+      const outerStartX = props.radius + Math.cos(toRadians(offsetAngle)) * props.radius;
+      const outerStartY = props.radius + Math.sin(toRadians(offsetAngle)) * props.radius;
+      const outerEndX = props.radius + Math.cos(toRadians(endAngle)) * props.radius;
+      const outerEndY = props.radius + Math.sin(toRadians(endAngle)) * props.radius;
+
+      const innerStartX = props.radius + Math.cos(toRadians(endAngle)) * props.innerRadius;
+      const innerStartY = props.radius + Math.sin(toRadians(endAngle)) * props.innerRadius;
+      const innerEndX = props.radius + Math.cos(toRadians(offsetAngle)) * props.innerRadius;
+      const innerEndY = props.radius + Math.sin(toRadians(offsetAngle)) * props.innerRadius;
+
+      const pathData = `
+        M ${outerStartX} ${outerStartY}
+        A ${props.radius} ${props.radius} 0 0 0 ${outerEndX} ${outerEndY}
+        L ${innerStartX} ${innerStartY}
+        A ${props.innerRadius} ${props.innerRadius} 0 0 1 ${innerEndX} ${innerEndY}
+        Z
+      `;
+
+      offsetAngle -= props.segmentAngle; // 逆时针调整角度
+      return {
+        pathData,
+        centerX,
+        centerY,
+        icon: segment.icon,
+        label: segment.label,
+        index
+      };
+    });
+  };
+
+  // 手指按下事件，记录初始角度
+  const onTouchStart = (event) => {
+    startTouchAngle = computedTouchAngle(event.touches[0]); // 获取初始触摸角度
+  };
+  // 手指移动事件，计算拖动角度
+  const onTouchMove = (event) => {
+    const currentTouchAngle = computedTouchAngle(event.touches[0]);
+
+    const offsetAngle = currentTouchAngle - startTouchAngle;
+
+    // 限制最大旋转角度
+    if (isAngleInRange()) {
+      deltaAngle = offsetAngle;
+      maxOffsetAngle = Math.floor(offsetAngle);
+    } else {
+      deltaAngle = maxOffsetAngle + (offsetAngle - maxOffsetAngle) * 0.3;
+    }
+    updatePaths(); // 更新路径和图标位置
+  };
   // 手指松开事件，结束拖动
   const onTouchEnd = () => {
-    lastTouchAngle = null;
-
-    // let endTouchAngle = getTouchAngle(event);
-
-    // setClockwiseOrAnticlockwise(startTouchAngle, endTouchAngle);
-
-    console.log(lastClockwiseOrAnticlockwise);
-
-    if (Math.abs(deltaAngle) > Math.abs(maxOffsetAngle.value)) {
-      reboundStartAngle = deltaAngle;
-      reboundTargetAngle = maxOffsetAngle.value; // 目标角度是0
-      reboundProgress = 0; // 回弹进度重置
-      isRebounding = true; // 开始回弹动画
-
-      animateRebound(); // 启动回弹动画
+    if (isAngleInRange()) {
+      afterByTouchEnd();
     } else {
-      startAngle.value = startAngle.value + deltaAngle;
-    }
-
-    console.log(deltaAngle, startAngle.value);
-  };
-
-  // 计算旋转方向
-  const getClockwiseOrAnticlockwise = (startAngle, endAngle) => {
-    // 计算角度差
-    let angleDifference = endAngle - startAngle;
-
-    // 确保角度差在 0-360 度之间
-    if (angleDifference < -180) {
-      angleDifference += 360;
-    } else if (angleDifference > 180) {
-      angleDifference -= 360;
-    }
-
-    // 判断顺时针还是逆时针
-    if (angleDifference > 0) {
-      console.log("顺时针旋转");
-      return "clockwise";
-      // lastClockwiseOrAnticlockwisekwise = "clockwise";
-    } else if (angleDifference < 0) {
-      console.log("逆时针旋转");
-      // lastClockwiseOrAnticlockwise = "anticlockwise";
-      return "anticlockwise";
+      // 启动回弹动画
+      animateRebound({
+        isRebounding: true,
+        reboundStartAngle: deltaAngle,
+        reboundTargetAngle: maxOffsetAngle,
+        reboundProgress: 0
+      });
     }
   };
-  updatePaths(); // 初始化路径和图标位置
+
+  // 展开和收缩动画
+  const animateExpandCollapse = (expand) => {
+    visible.value = expand;
+    anime({
+      targets: ".segment-group", // 使用 class 选择路径元素
+      duration: 500,
+      easing: "easeInOutQuad",
+      scale: expand ? [0, 1] : [1, 0], // 展开和收缩的缩放动画
+      opacity: expand ? [0, 1] : [1, 0]
+    });
+  };
+
+  onMounted(() => {
+    const rect = assistiveTouchRef.value.getBoundingClientRect();
+    assistiveTouchCenterCoords.value = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    };
+
+    updatePaths();
+  });
 </script>
 
 <template>
-  <svg :width="radius * 2" :height="radius * 2" style="touch-action: none">
+  <svg
+    ref="assistiveTouchRef"
+    :width="radius * 2"
+    :height="radius * 2"
+    :style="`touch-action: none; z-index: ${z}`"
+    class="fixed -right-26 bottom-1/3"
+  >
+    <defs>
+      <filter id="shadow" x="-50%" y="-50%" width="200%" height="200%">
+        <feDropShadow dx="0" dy="3" stdDeviation="12" flood-color="black" flood-opacity="0.2" />
+      </filter>
+      <filter id="path-shadow" x="-50%" y="-50%" width="200%" height="200%">
+        <feDropShadow dx="0" dy="0" stdDeviation="4" flood-color="black" flood-opacity="0.2" />
+      </filter>
+    </defs>
     <g
       v-for="(item, index) in paths"
       :key="index"
-      style="cursor: pointer"
-      @touchstart="onTouchStart"
-      @touchmove="onTouchMove"
-      @touchend="onTouchEnd"
-      @touchcancel="onTouchEnd"
+      :style="style"
+      class="segment-group"
+      @click="() => $emit('segment-tap', item)"
+      @touchstart.passive="onTouchStart"
+      @touchmove.passive="onTouchMove"
+      @touchend.passive="onTouchEnd"
+      @touchcancel.passive="onTouchEnd"
     >
       <!-- 画扇形 -->
-      <path :d="item.pathData" fill="blue" stroke="white" stroke-width="2" />
+      <path :d="item.pathData" class="fill-(--cm-primary-color)" filter="url(#path-shadow)" />
       <!-- 画 icon -->
       <image
         :x="item.centerX - 10"
-        :y="item.centerY - 30"
+        :y="item.centerY - 20"
         width="20"
         height="20"
         :href="item.icon"
@@ -238,9 +265,9 @@
       <!-- 画文本 -->
       <text
         :x="item.centerX"
-        :y="item.centerY + 10"
+        :y="item.centerY + 12"
         fill="white"
-        font-size="16"
+        font-size="12"
         text-anchor="middle"
         dominant-baseline="middle"
       >
@@ -248,7 +275,64 @@
       </text>
     </g>
 
-    <!-- 中心控制圆 -->
-    <circle :cx="radius" :cy="radius" r="40" fill="red" stroke="black" stroke-width="2" />
+    <!-- 组1：包含第一个圆、文字和图标 -->
+    <g @click="() => animateExpandCollapse(!visible)">
+      <!-- 中心控制圆 -->
+      <circle
+        :cx="radius - 20"
+        :cy="radius"
+        r="24"
+        class="fill-(--cm-primary-color)"
+        filter="url(#shadow)"
+      />
+
+      <transition name="fade" mode="out-in">
+        <!-- 画文本 -->
+        <text
+          v-if="!visible"
+          :x="radius - 20"
+          :y="radius"
+          fill="white"
+          font-size="13"
+          font-weight="bold"
+          text-anchor="middle"
+          dominant-baseline="middle"
+        >
+          <template v-if="name instanceof Array">
+            <tspan
+              v-for="(item, index) in name"
+              :key="index"
+              :x="radius - 20"
+              :dy="(index === 0 ? -6 : 16) * (name.length === 2)"
+            >
+              {{ item }}
+            </tspan>
+          </template>
+          <template v-else>
+            {{ name }}
+          </template>
+        </text>
+
+        <!-- 画 icon -->
+        <image v-else :x="radius - 32" :y="radius - 12" width="24" height="24" :href="Icon" />
+      </transition>
+    </g>
   </svg>
 </template>
+
+<style lang="scss" scoped>
+  .segment-group {
+    transform: scale(0);
+    cursor: pointer;
+  }
+
+  /* 定义过渡动画 */
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s ease;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
